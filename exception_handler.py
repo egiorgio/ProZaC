c14ddc5e4a01a6785d79d5a5e09bd28eed2e7a6d
Matteo Pergolesi <matpergo@gmail.com>
2015-08-20 16:21:40 +0200

    Simplified exception handling. Fixed a bug with Zabbix URL.

--- exception_handler.py
+++ exception_handler.py
@@ -11,25 +11,8 @@ import logging
 logger = logging.getLogger('ZCP')
 
 def handle_HTTPError_openstack(exception, caller):
-    message = ""
-    if exception.code == 401:
-      message = "Unauthorized by Openstack."
-    elif exception.code == 404:
-      message = "Openstack resource not found."
-    elif exception.code == 503:
-      message = "Openstack service unavailable."
-    else:
-      message = "Unknown error."
-    logger.warning("Error code %d: %s" % (code, message))
+    logger.warning("Zabbix Error %d: %s" % (exception.code, exception.reason))
 
 def handle_HTTPError_zabbix(exception, caller):
-    message = ""
-    if exception.code == 401:
-      message = "Unauthorized by Zabbix."
-    elif exception.code == 404:
-      message = "Zabbix resource not found."
-    elif exception.code == 503:
-      message = "Zabbix service unavailable."
-    else:
-      message = "Unknown error."
-    logger.warning("Error code %d: %s" % (code, message)) 
+    logger.warning("Zabbix Error %d: %s" % (exception.code, exception.reason))
+
